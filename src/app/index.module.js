9730dfd1beaf90c13cb6f9b1adf94d409b404be6
laurabcn <laurariera9@gmail.com>
2015-10-26 19:39:44 +0100

    First commit: yeoman-gulp-angular

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('amaliaLaFantastica', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ui.bootstrap', 'toastr']);
+
+})();
